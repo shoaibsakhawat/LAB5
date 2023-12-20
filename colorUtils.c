b62a198b4e3b9300ad187ebb02ac93924db2b442
shoaib sakhawat <shoaibkarlal23@gmail.com>
2023-12-21 01:00:27 +0500

    DECLARING INTEGERS A,B,C

--- colorUtils.c
+++ colorUtils.c
@@ -11,6 +11,22 @@ int max(int x, int y, int z) {
   return m;
 }
 
+
+int min(int a,int b,int c)
+{
+  int min=a;
+  if(b<min)
+  {
+    min=b;
+  }
+  if(c<min)
+  {
+    min=c;
+  }
+  return min;
+}
+
+
 int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
