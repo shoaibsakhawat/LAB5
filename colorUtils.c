0b83ef1ff06685f525daf77d7fbf047b5ec78e61
shoaib sakhawat <shoaibkarlal23@gmail.com>
2023-12-21 01:05:26 +0500

    VARIABLES NAMES AND ALSO RETURN THEIR VALUES

--- colorUtils.c
+++ colorUtils.c
@@ -31,3 +31,16 @@ int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
 }
+
+
+
+
+int toGrayScaleLightness(int red,int green,int blue)
+{
+  return(max(red,green,blue)+min(red,green,blue))/2;
+}
+int toGrayScaleLuminosity(int red,int green,int blue)
+{
+  return (int)(0.21*red+0.72*green+0.07*blue);//using (int) for rounding of!!
+}
+
